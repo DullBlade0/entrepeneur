9c35b7fdd0e5c5033971c1f577b98d9e921590a3
blade-satellite <dullblade0@gmail.com>
2016-10-24 03:27:14 -0500

    Up to video 19 of Coding Entrepeneurs

---
+++ posts/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .forms import PostForm
+from .models import Post
+# Create your views here.
+
+
+def posts_create(request):
+    if request.method == "POST":
+        print(request.POST)
+    context = {
+        'form': form
+    }
+    return render(request, 'post_form.html', context)
+
+def posts_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        'title': 'Detail',
+        'instance': instance
+    }
+    return render(request, 'post_detail.html', context)
+
+def posts_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, 'index.html', context)
+
+def posts_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def posts_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
