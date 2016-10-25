58f1e4a0e445898b3faf205e0dd5c0f4b21338b8
blade-satellite <dullblade0@gmail.com>
2016-10-25 01:45:49 -0500

    Finished the First Tutorial

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,22 @@
+from urllib.parse import quote_plus
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 # Create your views here.
 
 
 def posts_create(request):
-    form = PostForm(request.POST)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Succesfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -19,25 +25,57 @@ def posts_create(request):
     }
     return render(request, 'post_form.html', context)
 
-def posts_detail(request, id):
-    instance = get_object_or_404(Post, id=id)
+def posts_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+    share_string = quote_plus(instance.content)
     context = {
         'title': 'Detail',
-        'instance': instance
+        'instance': instance,
+        'share_string': share_string
     }
     return render(request, 'post_detail.html', context)
 
 def posts_list(request):
-    queryset = Post.objects.all()
+    today = timezone.now().date()
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) | 
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query) 
+            ).distinct()  
+    paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list": queryset,
-        "title": "List"
+        "title": "List",
+        'page_request_var': page_request_var,
+        'today': today
     }
     return render(request, 'post_list.html', context)
 
-def posts_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+def posts_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -51,6 +89,8 @@ def posts_update(request, id=None):
     return render(request, 'post_form.html', context)
 
 def posts_delete(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     instance.delete()
     messages.success(request, 'Successfully deleted!')
